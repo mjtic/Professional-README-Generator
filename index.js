267566450192b583adc2911b57d83596822568b2
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-01-20 19:36:52 -0600

    added userInput valildation in questions

--- index.js
+++ index.js
@@ -1,71 +1,122 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkDown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkDown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-      },
-      {
-        type: 'input',
-        message: 'What is the description of your project?',
-        name: 'description',
-      },
-      //table of conetents skip -due to need of static
-      {
-        type: 'input',
-        message: 'What is the installation instruction of your project?',
-        name: 'installation',
-      },
-      {
-        type: 'input',
-        message: 'What is the usage information of your project?',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'What is the contribution guideline of your project?',
-        name: 'contributor',
-      },
-      {
-        type: 'input',
-        message: 'What is the test instrcutions of your project?',
-        name: 'test',
-      },
-      {
-        type: 'list',
-        message: 'What is the license of your project?',
-        name: 'license',
-        choices:[
-          "MIT License","Apache License 2.0","GNU General Public License v3.0"
-        ]
-      },
-      {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
-      },
-      {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-      },
-
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the title of your project!");
+            return false;
+        }
+    }
+  },
+  {
+    type: "input",
+    message:
+      "What is the description of your project? Motivation? Why did you build this project? What problem does it solve? What did you learn?",
+    name: "description",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the description of your project!");
+            return false;
+        }
+    }
+  },
+  //table of conetents skip -due to need of static
+  {
+    type: "input",
+    message: "What is the installation instruction of your project?",
+    name: "installation",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the installation instruction of your project!");
+            return false;
+        }
+    }    
+  },
+  {
+    type: "input",
+    message: "What is the usage information of your project?",
+    name: "usage",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the usage information of your project!");
+            return false;
+        }
+    }   
+  },
+  {
+    type: "input",
+    message: "What is the contribution guideline of your project?",
+    name: "contributor",
+  },
+  {
+    type: "input",
+    message: "What is the test instructions of your project?",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "What is the license of your project?",
+    name: "license",
+    choices: [
+      "MIT License",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+    ],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the GitHub username of your project!");
+            return false;
+        }
+    }   
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+    validate: userInput => {
+        if(userInput){
+            return true;
+        }else{
+            console.log("please enter the email address of your project!");
+            return false;
+        }
+    }   
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-    .then((response)=>{
-        console.log(response);
-    })
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+  });
 }
 
 // Function call to initialize app
